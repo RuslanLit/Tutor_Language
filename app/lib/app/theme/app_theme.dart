a5049cde52277e3dab7fa1b7cd71f09e7b32519b
Ruс Li <arslanlitvin@gmail.com>
2026-08-04 15:53:43 +0300

    Add Kurale interface font support

--- app/lib/app/theme/app_theme.dart
+++ app/lib/app/theme/app_theme.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+const tutorLanguageFontFamily = 'Kurale';
+
 ThemeData buildLightTheme() {
   const seedColor = Color(0xFF2F6F73);
 
   return ThemeData(
     colorScheme: ColorScheme.fromSeed(seedColor: seedColor),
+    fontFamily: tutorLanguageFontFamily,
     useMaterial3: true,
   );
 }
